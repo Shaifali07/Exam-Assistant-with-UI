2cdbf75779b401b51142ab33b0b43415ce2d4757
Shaifali07 <spmalukani7@gmail.com>
2026-03-28 17:01:25 +0530

    Paper generation for External and sessional examination

--- llm.py
+++ llm.py
@@ -1,4 +1,4 @@
-def generate_paper(SUBJECT,COs,syllabus,instructions=' '):
+def generate_paper(SUBJECT,COs,Examination,syllabus,instructions=' '):
     from langchain_groq import ChatGroq
     from langchain_core.prompts import ChatPromptTemplate
     from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
@@ -23,7 +23,7 @@ def generate_paper(SUBJECT,COs,syllabus,instructions=' '):
         Marks:int=Field(description="Marks for question")
     parser=JsonOutputParser(pydantic_object=Question_Formation)
     format_instructions = parser.get_format_instructions()
-    prompt = ChatPromptTemplate.from_messages([
+    prompt_sessional = ChatPromptTemplate.from_messages([
             ("system", "You are an expert academic question paper setter. Generate the questions from the all over syllabus." ),
             ("human",'''Generate a university-level question paper based on the following details:
             INPUTS
@@ -32,7 +32,8 @@ def generate_paper(SUBJECT,COs,syllabus,instructions=' '):
             Syllabus: {syllabus}
             STRICT RULE:
             All the question must be from the syllabus
-            
+            DO NOT include question outside the syllabus
+            use course ourcomes {COs} only
             
             
             EXAM STRUCTURE
@@ -57,14 +58,15 @@ def generate_paper(SUBJECT,COs,syllabus,instructions=' '):
             Two options: Option 1 Option 2
             Each option contains: - Two questions (6 marks each) OR structured. If structured, structure must be same in both options
             Example:
-            Option 1 → CO3 (6 marks) + CO4 (2 marks)+ CO5 (2 marks)
-            Option 2 → CO3 + CO4
+            Option 1 → CO3 (6 marks) + CO4 (3 marks)+ CO5 (3 marks)
+            Option 2 → CO3 (6 marks) + CO4 (3 marks)+ CO5 (3 marks)
+                
+            following is Not allowed:
+            Option 1 → CO3 (6 marks) + CO4 (6 marks)
+             Option 2 → CO3 (6 marks) + CO4 (3 marks)+ CO5 (3 marks)
             
             STRICT RULE: - COs can be mixed WITHIN an option
             BUT both options MUST have SAME SET of COs
-            
-            VERY VERY STRICT RULE:
-            {instructions}
             
             Example:
             Option 1 → CO3 + CO4
@@ -72,6 +74,11 @@ def generate_paper(SUBJECT,COs,syllabus,instructions=' '):
             NOT allowed:
             Option 1 → CO2 + CO3
             Option 2 → CO1 + CO4
+            
+            VERY VERY STRICT RULE:
+            {instructions}
+            
+            
             MANDATORY REQUIREMENTS
             Each question must include: - Course Outcome (CO) - Bloom’s Level (Apply / Analyze / Evaluate)
             Maintain: - University-level difficulty - Balanced syllabus coverage
@@ -85,6 +92,111 @@ def generate_paper(SUBJECT,COs,syllabus,instructions=' '):
             give images if necessary
             
             GOAL Generate a fully structured, strictly CO-compliant, numerical-focused university question paper following ALL rules above. follow {format_instructions} ''')])
+    prompt_External = ChatPromptTemplate.from_messages([
+        ("system",
+         "You are an expert academic question paper setter. Generate the questions from the all over syllabus."),
+        ("human", '''Generate a university-level question paper based on the following details:
+                INPUTS
+                Subject: {SUBJECT}
+                Course Outcomes {COs}
+                Syllabus: {syllabus}
+                STRICT RULE:
+                All the question must be from the syllabus
+                DO NOT include question outside the syllabus
+                use course ourcomes {COs} only
+
+
+                EXAM STRUCTURE
+                Total Marks = 60
+                Total Questions = 6 (Q.1, Q.2, Q.3,Q.4, Q.5, Q.6) Each Question = 10 Marks
+                IMPORTANT CO RULES
+                Q.1 (Objective Type – 10 Marks) - 5 sub-questions (a–e),
+                each 2 marks - Must be numerical / analytical / problem-solving/ Evaluation- true/false and justification
+                CO Assignment: - Either all from same CO OR mixed COs - Prefer maximum CO coverage ---
+
+
+                Q.2 (Attempt ANY TWO out of THREE – 10 Marks) - Each question = 5 marks
+                Format: (5) OR (3+2)
+
+                STRICT RULE:
+                ALL THREE questions MUST belong to SAME CO
+                Example: Q.2(a) → CO2 Q.2(b) → CO2 Q.2(c) → CO2
+                Do NOT mix COs
+                Each question must: - Be numerical / application / algorithm-based /Evaluation/Application/programming- Have all sub-parts from SAME CO
+
+                Q.3 (Attempt ANY ONE option – 10 Marks)
+                Two options: Option 1 Option 2
+                Each option contains: - Two questions (5 marks each) OR structured. If structured, structure must be same in both options
+                Example:
+                Option 1 → CO3 (5 marks) + CO4 (3 marks)+ CO5 (2 marks)
+                Option 2 → CO3 (5 marks) + CO4 (3 marks)+ CO5 (2 marks)
+                following is not Not allowed
+                Option 1 → CO3 (5 marks) + CO4 (5 marks)
+                Option 2 → CO3 (5 marks) + CO4 (3 marks)+ CO5 (2 marks)
+                STRICT RULE: - COs can be mixed WITHIN an option
+                BUT both options MUST have SAME SET of COs
+
+                Example:
+                Option 1 → CO3 + CO4
+                Option 2 → CO3 + CO4
+                NOT allowed:
+                Option 1 → CO2 + CO3
+                Option 2 → CO1 + CO4
+                
+                Q.4 (Objective Type – 10 Marks) - 5 sub-questions (a–e),
+                each 2 marks - Must be numerical / analytical / problem-solving/ Evaluation- true/false and justification
+                CO Assignment: - Either all from same CO OR mixed COs - Prefer maximum CO coverage ---
+
+
+                Q.5 (Attempt ANY TWO out of THREE – 10 Marks) - Each question = 5 marks
+                Format: (5) OR (3+2)
+
+                STRICT RULE:
+                ALL THREE questions MUST belong to SAME CO
+                Example: Q.5(a) → CO2 Q.5(b) → CO2 Q.5(c) → CO2
+                Do NOT mix COs
+                Each question must: - Be numerical / application / algorithm-based /Evaluation/Application- Have all sub-parts from SAME CO
+
+                Q.6 (Attempt ANY ONE option – 10 Marks)
+                Two options: Option 1 Option 2
+                Each option contains: - Two questions (5 marks each) OR structured. If structured, structure must be same in both options
+                Example:
+                Option 1 → CO3 (5 marks) + CO4 (3 marks)+ CO5 (2 marks)
+                Option 2 → CO3 (5 marks) + CO4 (3 marks)+ CO5 (2 marks)
+                following is not Not allowed
+                Option 1 → CO3 (5 marks) + CO4 (5 marks)
+                Option 2 → CO3 (5 marks) + CO4 (3 marks)+ CO5 (2 marks)
+                STRICT RULE: - COs can be mixed WITHIN an option
+                BUT both options MUST have SAME SET of COs
+
+                Example:
+                Option 1 → CO3 + CO4
+                Option 2 → CO3 + CO4
+                NOT allowed:
+                Option 1 → CO2 + CO3
+                Option 2 → CO1 + CO4
+                VERY VERY STRICT RULE:
+                {instructions}
+
+                
+                MANDATORY REQUIREMENTS
+                Each question must include: - Course Outcome (CO) - Bloom’s Level (Apply / Analyze / Evaluate)
+                Maintain: - University-level difficulty - Balanced syllabus coverage
+                Focus on: - Numericals, application based questions, Problem-solving,programming
+                Avoid: - Pure theory-only questions
+
+
+                OUTPUT FORMAT - Clean, exam-ready format
+                Clearly labeled Q.1, Q.2, Q.3,Q.4,Q.5, Q.6
+                Marks for each question - CO + Bloom’s level mentioned
+                give images if necessary
+
+                GOAL Generate a fully structured, strictly CO-compliant, numerical-focused university question paper following ALL rules above. follow {format_instructions} ''')])
+    if Examination=="Sessional":
+        prompt=prompt_sessional
+    else:
+        prompt=prompt_External
+
     chain = prompt | llm | parser
     result = chain.invoke({"SUBJECT": SUBJECT,"COs":COs,"syllabus":syllabus, "format_instructions":format_instructions, "instructions":instructions})
     return result
--- main.py
+++ main.py
@@ -8,11 +8,12 @@ st.title("Your Exam Assistant 👩🏻‍💻")
 with st.form("paper_generator"):
    SUBJECT= st.text_input("Enter the Subject Name")
    COs=st.text_area("Enter Course Outcomes", height=150)
+   Examination=st.radio("Select Examination",["Sessional","External"])
    Syllabus=st.text_area("Enter Subject Syllabus",height=400)
    Insturctions=st.text_area("Enter the Instructions(if any)",height=150)
+
    submitted = st.form_submit_button("Submit")
    if submitted:
-        generated_paper=generate_paper(SUBJECT, COs, Syllabus,Insturctions)
-        df = pd.DataFrame(generated_paper)
-        # df.to_excel('paper.xlsx')
-        st.table(df)
+            generated_paper=generate_paper(SUBJECT, COs,Examination, Syllabus,Insturctions)
+            df = pd.DataFrame(generated_paper)
+            st.table(df)

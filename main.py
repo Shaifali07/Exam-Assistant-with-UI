911374c7e86a16449c08f1a86981706cbe3349c0
Shaifali07 <56667719+Shaifali07@users.noreply.github.com>
2026-03-30 09:44:21 +0000

    Testing- with helper text

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ with st.form("paper_generator"):
    COs=st.text_area("Enter Course Outcomes", height=150)
    Examination=st.radio("Select Examination",["Sessional","External"])
    Syllabus=st.text_area("Enter Subject Syllabus",height=400)
-   Insturctions=st.text_area("Enter the Instructions(if any)",height=150, value= '''Enter any additional rules or preferences for generating the question paper.
+   Insturctions=st.text_area("Enter the Instructions   (if any)",height=150, value= '''Enter any additional rules or preferences for generating the question paper.
                              Example:
                              1) All questions in Q.1 must be from CO2.
                              2) Both options in Q.3 must have the same CO(s) Allowed: Option 1 → CO4 + CO5, Option 2 → CO4 + CO5, Not allowed: Option 1 → CO3 + CO5, Option 2 → CO2 + CO4

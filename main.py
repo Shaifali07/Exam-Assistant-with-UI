a69218ae0552b21ca4414c3c6daf11ac1efef0bc
Shaifali07 <56667719+Shaifali07@users.noreply.github.com>
2026-04-01 06:38:07 +0000

    Regenerate button Fixed

--- main.py
+++ main.py
@@ -33,8 +33,9 @@ with st.form("paper_generator"):
             status_placeholder = st.empty()
             with st.spinner("Generating question paper... please wait ⏳"):
                 generated_paper = generate_paper(SUBJECT, COs, Examination, Syllabus, Insturctions, status_placeholder)
-            st.session_state["generated"] = True
+            
             df = pd.DataFrame(generated_paper)
+            st.session_state["generated"] = True
             st.session_state["df"] = df
             st.session_state["SUBJECT"] = SUBJECT
             st.session_state["COs"] = COs
@@ -42,15 +43,6 @@ with st.form("paper_generator"):
 
             # print("result returned")
             # st.table(df)
-if "df" in st.session_state:
-    if st.button("🗑️ Clear All"):
-        keys_to_clear = ["subject", "cos", "exam", "syllabus", "instructions", "df", "generated"]
-
-        for key in keys_to_clear:
-            if key in st.session_state:
-                del st.session_state[key]
-
-        st.rerun()
 # Create download button
 if "df" in st.session_state:
     df = st.session_state["df"]
